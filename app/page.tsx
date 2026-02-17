2b918fdc0954183eaaf5030d8fa35a5fef5440cc
gabrielhrabelo <gabrielrabeloti@gmail.com>
2026-02-17 13:19:26 -0300

    implement hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
   );
 }
